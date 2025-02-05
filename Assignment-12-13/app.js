d24bffb7b3c1c5c96581cbeafcc4b7577f40ac98
Mustafa Ahmed <mustufaahmed450@gmail.com>
2025-02-05 21:42:19 +0500

    modify ssignment12-13

--- Assignment-12-13/app.js
+++ Assignment-12-13/app.js
@@ -1,48 +1,48 @@
 //  Assignment-12-13
 
 // Question-1
-// let num = +prompt("Choose any number");
+let num = +prompt("Choose any number");
 
-// if (num % 3 === 0){
-//     console.log("Number is divisible by 3");
-// }
-// else {
-//     console.log ("Number is not divisible by 3");
-// }
+if (num % 3 === 0){
+    console.log("Number is divisible by 3");
+}
+else {
+    console.log ("Number is not divisible by 3");
+}
 
 
 
 // Question-2
-// let num1 = +prompt("Choose any number");
+let num1 = +prompt("Choose any number");
 
-// if (num1 % 2 === 0){
-//     console.log("even");
-// }
-// else {
-//     console.log ("odd");
-// }
+if (num1 % 2 === 0){
+    console.log("even");
+}
+else {
+    console.log ("odd");
+}
 
 
 
 // Question-3
-// let age = +prompt("Your age");
+let age = +prompt("Your age");
 
-// if (age > 18){
-//     console.log("Old Enough");
-// }
-// else {
-//     console.log ("Too Young");
-// }
+if (age > 18){
+    console.log("Old Enough");
+}
+else {
+    console.log ("Too Young");
+}
 
 
 
 // Questiom-4
-// let myName = "Mustafa";
-// let yourName = prompt("Your Name");
+let myName = "Mustafa";
+let yourName = prompt("Your Name");
 
-// if (myName === yourName) {
-//     console.log("Hey! We have same name");
-// }
+if (myName === yourName) {
+    console.log("Hey! We have same name");
+}
 
 
 // Question-5
@@ -64,116 +64,116 @@
 
 
 // Question-7
-// let number1 = +prompt("Choose first number");
-// let number2 = +prompt("Choose second number");
-// let operation = prompt("Choose operator");
-// if( operation === '+'){
-//     console.log(number1 + number2);
-// }
-// else if( operation === '-'){
-//     console.log(number1 - number2);
-// }
-// else if( operation === '%'){
-//     console.log(number1 % number2);
-// }
-// else if( operation === '*'){
-//     console.log(number1 * number2);
-// }
-// else if( operation === '/'){
-//     console.log(number1 / number2);
-// }
+let number1 = +prompt("Choose first number");
+let number2 = +prompt("Choose second number");
+let operation = prompt("Choose operator");
+if( operation === '+'){
+    console.log(number1 + number2);
+}
+else if( operation === '-'){
+    console.log(number1 - number2);
+}
+else if( operation === '%'){
+    console.log(number1 % number2);
+}
+else if( operation === '*'){
+    console.log(number1 * number2);
+}
+else if( operation === '/'){
+    console.log(number1 / number2);
+}
 
 
 
 //  Question-8
-// let time = +prompt("Enter a time");
-// if ( time >= 100 && time < 1200){
-//     console.log ("Good Morning!");
-// }
-// else if ( time >= 1200 && time < 1700){
-//     console.log ("Good Afternoon!");
-// }
-// else if ( time >= 1700 && time < 2100){
-//     console.log ("Good Evening!");
-// }
-// else if ( time >= 2100 && time < 2359){
-//     console.log ("Good Night!");
-// }
+let time = +prompt("Enter a time");
+if ( time >= 100 && time < 1200){
+    console.log ("Good Morning!");
+}
+else if ( time >= 1200 && time < 1700){
+    console.log ("Good Afternoon!");
+}
+else if ( time >= 1700 && time < 2100){
+    console.log ("Good Evening!");
+}
+else if ( time >= 2100 && time < 2359){
+    console.log ("Good Night!");
+}
 
 
 // Question-9
-// let leapYear = +prompt("YYYY");
-// if (leapYear === 2023 , 2019){
-//     console.log("Is a leap Year");
-// }
-// else {
-//     console.log ("Is not a leap year");
-// }
+let leapYear = +prompt("YYYY");
+if (leapYear === 2023 , 2019){
+    console.log("Is a leap Year");
+}
+else {
+    console.log ("Is not a leap year");
+}
 
 
 
 // Question-10
-// let myPassword = "Mustafa123";
-// let password = prompt("Enter a Password");
-// let confPassword = prompt("Confirm Password");
-// if ( myPassword === password){   
-//     console.log("Correct");  
-// }
-// else if (password !== myPassword) {
-//     console.log("Not correct password");
-// }
+let myPassword = "Mustafa123";
+let password = prompt("Enter a Password");
+let confPassword = prompt("Confirm Password");
+if ( myPassword === password){   
+    console.log("Correct");  
+}
+else if (password !== myPassword) {
+    console.log("Not correct password");
+}
 
 
 // Question-11
-// let firstName = prompt("Your name");
-// if (firstName === "Fahad") {
-//  console.log("Hello Fahad!");
-// }
-// else if (firstName !== "Fahad") {
-//  console.log("You are not Fahad!");
-// }
+let firstName = prompt("Your name");
+if (firstName === "Fahad") {
+ console.log("Hello Fahad!");
+}
+else if (firstName !== "Fahad") {
+ console.log("You are not Fahad!");
+}
 
 
 // Question-12
-// let hour = +prompt("What hour is it");
-//  if (hour < 18) {
-//     console.log("Good day!");
-// }
-// else{
-//     console.log("Good evening!");
-// }
+let hour = +prompt("What hour is it");
+ if (hour < 18) {
+    console.log("Good day!");
+}
+else{
+    console.log("Good evening!");
+}
 
 
 
 // Question-13
-// let chooseNum = +prompt("Choose a number");
-// if ( chooseNum > 0){
-//     console.log("The number is positive");
-// }
-// else if ( chooseNum < 0){
-//     console.log("The number is negative");
-// }
-// else {
-//     console.log("The number is zero");
-// }
+let chooseNum = +prompt("Choose a number");
+if ( chooseNum > 0){
+    console.log("The number is positive");
+}
+else if ( chooseNum < 0){
+    console.log("The number is negative");
+}
+else {
+    console.log("The number is zero");
+}
 
 
 
 
 //  Question-14
-// let timeHour = +prompt("Enter a time 0-23");
-// if ( timeHour >= 6 && timeHour < 9){
-//     console.log ("Breakfast is served");
-// }
-// else if ( timeHour >= 11 && timeHour < 13){
-//     console.log ("Time for Lunch");
-// }
-// else if ( timeHour >= 17 && timeHour < 20){
-//     console.log ("It's Dinner time");
-// }
-// else {
-//     console.log("Sorry, you'll have to wait, or go get a snack.");
-// }
+let timeHour = +prompt("Enter a time 0-23");
+if ( timeHour >= 6 && timeHour < 9){
+    console.log ("Breakfast is served");
+}
+else if ( timeHour >= 11 && timeHour < 13){
+    console.log ("Time for Lunch");
+}
+else if ( timeHour >= 17 && timeHour < 20){
+    console.log ("It's Dinner time");
+}
+else {
+    console.log("Sorry, you'll have to wait, or go get a snack.");
+}
 
 
 
@@ -195,3 +195,42 @@ else if ( yourNum !== 8){
     console.log( yourNum + " is not equal to 8");
     
 }
+
+
+
+// Question-17
+let obtainedMarks = +prompt('enter obtained marks');
+let totalMarks = +prompt('enter total marks');
+let percentag = obtainedMarks / totalMarks * 100;
+let grade, remarks;
+if (percentag >= 80) {
+    grade = "A-1";
+    remarks = "Excellant";
+}
+else if (percentag >= 70 && percentag < 80) {
+    grade = "A";
+    remarks = "Weldone";
+}
+else if (percentag >= 60 && percentag < 70) {
+    grade = "B";
+    remarks = "Good";
+}
+else if (percentag >= 50 && percentag < 60) {
+    grade = "C";
+    remarks = "You need improvement.";
+}
+else if (percentag >= 45 && percentag < 50) {
+    grade = "D";
+    remarks = "You need bht sari improvement.";
+}
+else if (percentag >= 33 && percentag < 45) {
+    grade = "E";
+    remarks = "poor performance.";
+}
+else {
+    grade = "F";
+    remarks = "lakh di lanat";
+}
+document.write("Grade: " + grade);
+document.write("<br />");
+document.write("Remarks: " + remarks);
