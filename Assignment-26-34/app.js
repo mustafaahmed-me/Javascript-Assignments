4678aa5011e21e920a05619aff3615730a2f11fc
Mustafa Ahmed <160065401+mustafaahmed-me@users.noreply.github.com>
2025-03-14 01:27:13 +0500

    Assignment-26-34

---
+++ Assignment-26-34/app.js
@@ -0,0 +1,181 @@
+// Assignment-26-34
+
+
+
+// Question-1 Positive
+// let num = +prompt("Enter any number");
+// console.log("Number " + num);
+// console.log("Round Of " + Math.round(num));
+// console.log("Floor " + Math.floor(num));
+// console.log("Ceil " + Math.ceil(num));
+
+
+
+// Question-2 Negative
+// let num = +prompt("Enter any number");
+// console.log("Number " + num);
+// console.log("Round Of " + Math.round(num));
+// console.log("Floor " + Math.floor(num));
+// console.log("Ceil " + Math.ceil(num));
+
+
+
+// Question-3 Positive
+// let nums = +prompt("Enter any number");
+// console.log("Number " + nums);
+// console.log("Round Of " + Math.round(nums));
+// console.log("Floor " + Math.floor(nums));
+// console.log("Ceil " + Math.ceil(nums));
+
+
+
+// Question-4 Negative
+// let nums = +prompt("Enter any number");
+// console.log("Number " + nums);
+// console.log("Round Of " + Math.round(nums));
+// console.log("Ceil " + Math.ceil(nums));
+// console.log("Floor " + Math.floor(nums));
+
+
+
+// Question-5
+// let ranDies = Math.ceil(Math.random() * 6);
+// console.log(`Rndom Dies value  ${ranDies}`);
+
+
+
+// Question-6
+// let imgEl = document.querySelectorAll("img")[0];
+
+// function changeCoin() {
+//     let ranNum = Math.ceil(Math.random() * 2);
+//     console.log(ranNum);
+    
+//     if (ranNum === 1) {
+//         imgEl.src = "images/five.png";
+//         console.log("Random Coin Value : Heads");
+        
+//     }
+//     else if (ranNum === 2) {
+//         imgEl.src = "images/chaand.png";
+//         console.log("Random Coin Value : Tails " );
+//     }
+// }
+
+
+
+// Question-7
+// let rNum = Math.floor(Math.random() * 100) + 1;
+// console.log(`Random number between 1 and 100: ${rNum}`);
+// console.log(`Random number between 1 and 100: ${rNum}`);
+
+
+
+// Question-8
+// let nums = Math.floor(Math.random() * 10) + 1;
+// let numer = +prompt("Write any number between 1 and 10");
+// // console.log(nums);
+
+// if (nums === numer) {
+//     console.log("Congrats the user");
+// }
+// else {
+//     console.log("Try Again");
+    
+// }
+
+
+
+// Question-9
+// let now = new Date();
+// console.log(now);
+
+
+
+// Question-10
+// month = new Date().getMonth();
+// let monthsName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+// console.log("Current Month" + month);
+// console.log(monthsName[month]);
+
+
+
+// Question-11
+// let today = new Date().getDay();
+// let daysName = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// let inputDay = prompt("Today day name?");
+// // console.log(today);
+// console.log("Today is " + daysName[today]);
+
+
+
+// Question-12
+// let nowDay = new Date().getDay();
+// let daysNames = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// console.log(`Today is ${daysNames[nowDay]}`);
+
+
+
+
+// Question-13
+// let day = new Date().getDay();
+// let dayssName = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// let todayDay = prompt("What day is today");
+
+// if (dayssName[day] === todayDay) {
+//     console.log("It's Fun day");
+// }
+// else{
+//     console.log("It's not Fun day");
+// }   
+
+
+
+// Question-14
+// let dateToday = new Date().getDate();
+
+// if (dateToday < 16) {
+//     console.log("First Fifteen days of the month");
+// }
+// else{
+//     console.log("Last days of the month");
+    
+// }
+
+
+
+// Question-15
+// let crntDate = new Date();
+// console.log(`Current date : ${crntDate}`);
+// let msTime = new Date("January 2 , 1970");
+// console.log(`Elapsed milliseconds since January 1, !970 : ${msTime/1000}`);
+// console.log(`Elapsed minutes since January 1, !970 : ${msTime/1000/60}`);
+
+
+
+// Question-16
+// let timeToday = new Date().getTime();
+
+// if (timeToday < 12) {
+//     console.log("Its AM ");
+// }
+// else{
+//     console.log("Its PM");
+// }
+
+
+
+// Question-17
+// let laterDate = new Date("December 31 , 2020");
+// console.log(laterDate);
+
+
+
+// Question-18
+// let ramdanPast = new Date("June 18 ,2015");
+// let nowaDay = new Date();
+// let howDay = nowaDay - ramdanPast;
+
+// console.log(`${Math.floor(howDay/1000/60/60/24)} days have sinced passed 1st Ramadan, 2015`);
+
+
